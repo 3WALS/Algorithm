41064f275a8b2f0cdb711d6a918085a50f150a22
JISEUNG ROH <jiseung.roh@gmail.com>
2017-03-19 22:29:10 +0900

    fix isSubsetOf, can't solve advanced version

--- 03-isSubsetOf/isSubsetOf.js
+++ 03-isSubsetOf/isSubsetOf.js
@@ -31,3 +31,67 @@ Array.prototype.isSubsetOf = function (arr) {
   return true;
 };
 
+// Advanced
+// Also work for arrays that contain objects and/or arrays
+Array.prototype.isSubsetOf = function (arr) {
+  var normalCheck = [],
+      reCheck = [];
+
+  function whatType(item) {
+    return Object.prototype.toString.call(item).slice(8,-1).toLowerCase();
+  }
+
+  function arrCheck(a, b) {
+    if ( a.length !== b.length ) return false;
+    for ( var i = 0; i<a.length; i++ ) {
+      if ( a[i] !== b[i] ) {
+        if ( whatType(a[i]) === 'array' ) { arrCheck(a[i], b[i]); }
+        else if ( whatType(a[i]) === 'object' ) { objCheck(a[i], b[i]); }
+        else return false;
+      }
+    }
+    // return true;
+  }
+
+  function objCheck(a, b) {
+    for ( var key in a ) {
+      if ( b[key] === undefined ) return false;
+      if ( a[key] !== b[key]) {
+        if ( whatType(a[i]) === 'array' ) { arrCheck(a[i], b[i]); }
+        else if ( whatType(a[i]) === 'object' ) { objCheck(a[i], b[i]); }
+        else return false;
+      }
+    }
+  }
+
+  this.forEach(function(item) {
+    if ( whatType(item) === 'array' || whatType(item) === 'object' ) { reCheck.push(item); }
+    else { normalCheck.push(item); }
+  });
+
+  if ( reCheck.length ) {
+    console.log('recheck executed', reCheck);
+    // only if it contains array || objects
+    reCheck.forEach(function(item) {
+      arrCheck(item, )
+    });
+  }
+
+  for ( var i = 0; i<this.length; i++ ) {
+    if ( arr.indexOf(this[i]) < 0 ) {
+      return false;
+    }
+  }
+  return true;
+
+};
+
+let product = {
+  productName   : 'TV',
+  maker         : 'LG',
+  features      : ['Time Recoding', 'Sharing Screen', 'Speech Recognition'],
+  productionYear: 2017
+};
+
+let { productName, maker, features } = product;
+
